7bb37e505e82d9ca57f7f8a4c38ff1d23b6341c3
Neelufer <ssneelu13@gmail.com>
2023-11-08 02:19:30 +0530

    Welcome to Employee Wage Computation Program

---
+++ src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
@@ -0,0 +1,7 @@
+package EMPLOYEE_WAGE_USECASES;
+
+public class Employee_Wage {
+    public static void main(String[] args){
+        System.out.println("Welcome To Employee Wage Computation Program");
+    }
+}
