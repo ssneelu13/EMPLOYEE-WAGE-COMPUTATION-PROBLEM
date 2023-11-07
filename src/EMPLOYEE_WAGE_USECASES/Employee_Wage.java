9039a22866e4a908371daf6146a578a7f87d9811
Neelufer <ssneelu13@gmail.com>
2023-11-08 02:23:54 +0530

    Check Employee is Present or Absent

--- src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
+++ src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
@@ -3,5 +3,13 @@ package EMPLOYEE_WAGE_USECASES;
 public class Employee_Wage {
     public static void main(String[] args){
         System.out.println("Welcome To Employee Wage Computation Program");
+        int max=1;
+        int min=0;
+        int present=(int)(Math.random()*(max-min+1));
+        if(present==1){
+            System.out.println("employee is present");
+        } else{
+            System.out.println("employee is absent");
+        }
     }
 }
