ea6318c351c9c7e310165a333d1aa87385ba7f07
Neelufer <ssneelu13@gmail.com>
2023-11-08 02:28:49 +0530

    Calculate Daily Employee Wage

--- src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
+++ src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
@@ -11,5 +11,16 @@ public class Employee_Wage {
         } else{
             System.out.println("employee is absent");
         }
+        int wage_per_hr = 20;//per hr pay
+        int full_day_hr = 8;//total no of hrs
+        present = (int) (Math.random()*(max-min+1));
+        if (present == 1) {
+            System.out.println("employee is present");
+            int salary=(wage_per_hr*full_day_hr);
+            System.out.println("salary is:"+salary);
+        }
+        else{
+            System.out.println("employee is absent and salary is zero");
+        }
     }
 }
