025f39a52377e45b5f961478349c6271256988e7
Neelufer <ssneelu13@gmail.com>
2023-11-08 09:09:17 +0530

    Solving using Switch Case Statement

--- src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
+++ src/EMPLOYEE_WAGE_USECASES/Employee_Wage.java
@@ -5,7 +5,7 @@ public class Employee_Wage {
         System.out.println("Welcome To Employee Wage Computation Program");
         int max=1;
         int min=0;
-        int present=(int)(Math.random()*(max-min+1));
+        int present=(int)(Math.random()*(max-min+1));//case 1:Check Employee is Present or Absent
         if(present==1){
             System.out.println("employee is present");
         } else{
@@ -13,7 +13,7 @@ public class Employee_Wage {
         }
         int wage_per_hr = 20;//per hr pay
         int full_day_hr = 8;//total no of hrs
-        present = (int) (Math.random()*(max-min+1));
+        present = (int) (Math.random()*(max-min+1));//case 2:Calculate Daily Employee Wage
         if (present == 1) {
             System.out.println("employee is present");
             int salary=(wage_per_hr*full_day_hr);
@@ -23,7 +23,7 @@ public class Employee_Wage {
             System.out.println("employee is absent and salary is zero");
         }
         wage_per_hr = 20;//per hr pay
-        int full_time = (int) (Math.random()*(max-min+1));
+        int full_time = (int) (Math.random()*(max-min+1));//case 3:Add Part time Employee & Wage
         if (full_time == 1) {
             System.out.println("full time employee");
             int full_time_hrs=16;
@@ -35,6 +35,28 @@ public class Employee_Wage {
             int part_time_hrs=8;
             int salary=(wage_per_hr*part_time_hrs);
             System.out.println("salary is:"+salary);
+        }
+        int employee_type;
+        max=1;
+        min=0;
+        employee_type=(int) (Math.random()*(max-min+1));//case 4:Solving using Switch Case Statement
+        wage_per_hr = 20;
+        int part_time_hrs=8;
+        int full_time_hrs=16;
+        switch(employee_type) {
+            case 1:
+                System.out.println("part time employee");
+                int salary = (wage_per_hr * part_time_hrs);
+                System.out.println("salary: "+salary);
+                break;
+            case 2:
+                System.out.println("full time employee");
+                salary = (wage_per_hr * full_time_hrs);
+                System.out.println("salary: "+salary);
+                break;
+            default:
+                System.out.println("not an employee");
+                break;
         }
     }
 }
